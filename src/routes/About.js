c07bfb786d71de08ed439130e2c74e5898f9a8b6
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-03-25 20:01:26 +0530

    design hero section

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 function About() {
   return (
     <div>
-            <h1>About</h1>
+      <Navbar />
+      <Hero
+        cName="hero-mid"
+        heroImg="https://images.unsplash.com/photo-1571624436279-b272aff752b5?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        title="About Us"
+        text="We are a team of professionals"
+      />
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/routes/Service.js
+++ src/routes/Service.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from "../components/Hero";
 
 function Service() {
   return (
     <div>
-            <h1>Service</h1>
+            <Navbar/>
+             <Hero 
+                   cName="hero-mid"
+                   heroImg="https://images.unsplash.com/photo-1502920514313-52581002a659?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                   title="Our Services"
+                    text="We provide the best services"
+             
+                   />
     </div>
   )
 }
