e8bda34d68883dcbc5a938b0dec28045fdead864
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-03-27 15:51:29 +0530

    card

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -8,14 +8,22 @@ function ImageCard(props) {
 
       
       <div style={{display: "flex", justifyContent: "center"}}>
-      <Card style={{ width: '18rem', height: "600px", margin: "10px" }}>
+      <Card style={{ width: '18rem', height: "600px", margin: "10px", boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", transition: "0.3s", borderRadius: "15px", overflow: "hidden", cursor: "pointer" }}
+       onMouseEnter={(e) => {
+        e.currentTarget.style.transform = "scale(1.09)"; // Slight zoom on hover
+        e.currentTarget.style.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.3)"; // Enhanced shadow on hover
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.transform = "scale(1)"; // Reset zoom
+        e.currentTarget.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)"; // Reset shadow
+      }} >
       <Card.Img variant="top" style={{height: "300px"}}  src={props.src} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>
           {props.text}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+       
       </Card.Body>
     </Card>
       </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -43,34 +43,42 @@ function Home() {
       <Card
         src="https://th.bing.com/th/id/OIP.iq3i4QDFNpg9oEWp-wrKkwHaEy?rs=1&pid=ImgDetMain"
         title="Malaysia"
-        text="From seeing a geisha show or walking the famous Fushimi Inari shrine in Kyoto to exploring
-       the bustling metropolis of Tokyo or staring out at Mount Fuji,
-       here’s our list of the best places to visit in Japan."
+        text="Malaysia is a country in Southeast Asia. it consists of 13 states and three federal territories, 
+        separated by the South China Sea into two regions: Peninsular Malaysia and Borneo's East Malaysia. 
+        Peninsular Malaysia shares a land and maritime border  "
       />
     </div>
     <div className="col-md-4 col-sm-6 col-lg-3 mb-4">
       <Card
-        src="https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2023/09/29193141/lake-kawaguchi.jpeg"
+        src="https://th.bing.com/th/id/OIP.xW-IIGIC3auZH6lOKXixsAHaF7?rs=1&pid=ImgDetMain"
         title="Thailand"
         text="From the vibrant cities of Bangkok and Chiang Mai to the island escapes of Phuket and Ko Samui, here are the best places to visit in Thailand."
       />
     </div>
     <div className="col-md-4 col-sm-6 col-lg-3 mb-4">
       <Card
-        src="https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2023/09/29193141/lake-kawaguchi.jpeg"
-        title="Japan"
-        text="From seeing a geisha show or walking the famous Fushimi Inari shrine in Kyoto to exploring
-       the bustling metropolis of Tokyo or staring out at Mount Fuji,
-       here’s our list of the best places to visit in Japan."
+        src="https://th.bing.com/th/id/OIP.QMjksFopZFkL26Z0PvDsDAHaFj?rs=1&pid=ImgDetMain"
+        title="Maldives"
+        text="The best islands in the Maldives include Rasdhoo for snorkeling with sharks, Ukulhas for a local, 
+        family-friendly vibe, and Dhigurah for luxury resorts.
+         Fonimagoodhoo is ideal for diving, while Maafushi offers affordability and excursions."
       />
     </div>
     <div className="col-md-4 col-sm-6 col-lg-3 mb-4">
       <Card
-        src="https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2023/09/29193141/lake-kawaguchi.jpeg"
-        title="Japan"
-        text="From seeing a geisha show or walking the famous Fushimi Inari shrine in Kyoto to exploring
-       the bustling metropolis of Tokyo or staring out at Mount Fuji,
-       here’s our list of the best places to visit in Japan."
+        src="https://th.bing.com/th/id/OIP.b3iFMAY9ZRZmAEv7MQ6oJAHaFG?rs=1&pid=ImgDetMain"
+        title="Sri Lanka"
+        text="Sri Lanka is a popular tourist destination. Tourism is a key industry that attracts international tourists yearly.
+         Foreigners visit Sri Lanka to see nature, wildlife, historical monuments, and indigenous culture. 
+        In 2018, tourist arrivals peaked at 2.5 million."
+      />
+    </div>
+    <div className="col-md-4 col-sm-6 col-lg-3 mb-4">
+      <Card
+        src="https://th.bing.com/th/id/OIP.JZj8lHhDVDIjWz5VFGzu-QHaFe?rs=1&pid=ImgDetMain"
+        title="Europe"
+        text="Europe attracts more tourists than any other continent: over 600 million international visitors annually, more than half of the global market.
+         Out of Earth's ten most visited countries, seven are in Europe, with good reason."
       />
     </div>
   </div>
